0557a7a4739464077b351c9a0121f3ef6288ec67
Dayloc <158208881+Dayloc@users.noreply.github.com>
2024-07-04 15:40:45 +0000

    nada

--- src/models.py
+++ src/models.py
@@ -2,18 +2,33 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(120), nullable=False)
+    apellido = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    fecha_subscripcion = db.Column(db.DateTime, nullable=False)
+    favoritos = db.relationship('Favorito', backref='usuario', lazy=True)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+class Personaje(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(120), nullable=False)
+    genero = db.Column(db.String(120), nullable=False)
+    nacimiento = db.Column(db.String(120), nullable=False)
+    favoritos = db.relationship('Favorito', backref='personaje', lazy=True)
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Planeta(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(120), nullable=False)
+    clima = db.Column(db.String(120), nullable=False)
+    terreno = db.Column(db.String(120), nullable=False)
+    favoritos = db.relationship('Favorito', backref='planeta', lazy=True)
+
+class Favorito(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
+    personaje_id = db.Column(db.Integer, db.ForeignKey('personaje.id'), nullable=True)
+    planeta_id = db.Column(db.Integer, db.ForeignKey('planeta.id'), nullable=True)
+
+
